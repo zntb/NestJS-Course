8f12d2cfd23dd8f1b5228a08816e5e761959abaa
zntb <zenetibi@yahoo.com>
2025-11-10 13:27:49 +0200

    9-Built in Pipes of Nest JS - 2 - pagination(limit, page)

--- 1-nest-js-intro/src/users/users.controller.ts
+++ 1-nest-js-intro/src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import {
   Controller,
+  DefaultValuePipe,
   Get,
   Param,
   ParseIntPipe,
@@ -11,15 +12,19 @@ import { User, UsersService } from './users.service';
 // http://localhost:3000/users
 @Controller('users')
 export class UsersController {
+  userService: UsersService;
+
+  constructor() {
+    this.userService = new UsersService();
+  }
+
   @Get()
-  getUsers(@Query() query: { gender: string }): User[] {
-    const userService = new UsersService();
-    if (query.gender) {
-      return userService
-        .getAllUsers()
-        .filter((user) => user.gender === query.gender);
-    }
-    return userService.getAllUsers();
+  getUsers(
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ): User[] {
+    console.log(limit, page);
+    return this.userService.getAllUsers();
   }
 
   @Get(':id')
