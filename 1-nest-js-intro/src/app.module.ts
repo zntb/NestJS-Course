dd65aab4cbab2ad1e518890821930dc57fd56277
zntb <zenetibi@yahoo.com>
2025-11-10 08:28:15 +0200

    2-How to Create a Module

--- 1-nest-js-intro/src/app.module.ts
+++ 1-nest-js-intro/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './users/users.module';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TweetModule],
   controllers: [AppController],
   providers: [AppService],
 })
