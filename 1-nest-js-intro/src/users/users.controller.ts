fd24c6eda1ff66a624262783cf1fc000eba4e253
zntb <zenetibi@yahoo.com>
2025-11-10 10:14:50 +0200

    4-Working with Route Decorators

--- 1-nest-js-intro/src/users/users.controller.ts
+++ 1-nest-js-intro/src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 // http://localhost:3000/users
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return 'You made a GET request to get all users';
+  }
+
+  @Post()
+  createUser() {
+    return 'You made a POST request to create a user';
+  }
+}
