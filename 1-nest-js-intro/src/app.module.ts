adc25a95fb87928d74dc61f149899fbeaf5305bd
zntb <zenetibi@yahoo.com>
2025-11-10 07:34:01 +0200

    1-Create & Run Nest JS Application

--- 1-nest-js-intro/src/app.module.ts
+++ 1-nest-js-intro/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SongsModule } from './songs/songs.module';
 
 @Module({
-  imports: [SongsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
